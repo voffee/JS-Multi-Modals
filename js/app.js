e21e0d143885f79ff3139f0d4cb8a3827474619a
Val <60014136+voffee@users.noreply.github.com>
2026-03-19 23:04:26 +0800

    created event listener for buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 
 const dialog = document.querySelector(`.modal`);
 
+const cardButtons = document.querySelectorAll(`.card button`);
+
+
+cardButtons.forEach(button => button.addEventListener('click', () => console.log(`You Clicked a Button!`)));
